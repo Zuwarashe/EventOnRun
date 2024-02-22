9d2c83d8eccf189994ea3451526aaba6900cc8f6
lab_services_student <lab_services_student@labVMH8OX>
2024-02-22 14:31:31 +0200

    Add project files.

---
+++ EventOnRun/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventOnRun
+{
+
+    //delegate void (string Data);
+
+
+    public partial class Form1 : Form
+    {
+
+        //---------------------------------------------------------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// 
+        /// </summary>
+        /// 
+
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+       
+
+        private void Form_DataSender(string data)
+        {
+            this.textBox1.Text = data;
+        }
+
+        private void Action_Click(object sender, EventArgs e)
+        {
+            var form = new PW(textBox1.Text);
+           // form.DataSender += Form_DataSender;
+            form.ShowDialog();
+        }
+    }
+}//----------------------------------------------------------------------------- end of file ------------------------------------------------------------//
---
+++ EventOnRun/PW.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventOnRun
+{
+
+   
+    public partial class PW : Form
+    {
+      /*  public event MyDataSender DataSender;
+         protected virtual void OnDataSender(string data)
+        {
+            DataSender? Invoke(data);
+        }*/
+
+        public PW(string dataIn)
+        {
+            InitializeComponent();
+            label1.Text = dataIn;
+        }
+
+       
+    }
+}
